6fb2c1981826d3ceac9ef1dbb33a7b7d8fc25a38
LoiLoL421 <j.robleslecona@ugto.mx>
2024-08-06 09:30:32 -0600

    debería funcionar al traer informacion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log ('333 conectando....') // estos 333 son para identificar que son mis console log
+
+    document.addEventListener('DOMContentLoaded', () => {
+    loadData(city = null)
+    })
+
+    const loadData = async (city) => {
+        if (!city) {
+            city = 'new york'
+        }
+    }
+    const url = `https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=${city}&limit=2`;
+    const options = {
+	    method: 'GET',
+	    headers: {
+		'x-rapidapi-key': 'Sign Up for Key',
+		'x-rapidapi-host': 'realty-in-us.p.rapidapi.com'
+	    }
+    };
+    
+    try {
+	    const response = await fetch (url, options);
+	    const result = await response.json();
+	    console.log('333 result =>', result);
+    } catch (error) {
+	    console.error(error);
+    }
